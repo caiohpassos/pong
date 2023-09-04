daf187020e5718c9a8e0f8d66193a6fea4f1bf5e
Caio Passos <62894010+caiohpassos@users.noreply.github.com>
2023-09-04 20:14:26 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,137 @@
+//variáveis da bolinha
+let xBolinha = 300;
+let yBolinha = 200;
+let diametro = 20;
+let raio = diametro / 2;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+
+let colidiu = false;
+
+//placar do jogo
+let meusPontos = 0;
+let pontosDoOponente = 0;
+
+//sons do jogo
+let raquetada;
+let ponto;
+let trilha;
+
+function preload(){
+  trilha = loadSound("trilha.mp3");
+  ponto = loadSound("ponto.mp3");
+  raquetada = loadSound("raquetada.mp3");
+}
+
+//variáveis do oponente
+let xRaqueteOponente = 585;
+let yRaqueteOponente = 150;
+
+function setup() {
+  createCanvas(600, 400);
+  trilha.loop();
+}
+
+function draw() {
+    background(0);
+    mostraBolinha();
+    movimentaBolinha();
+    verificaColisaoBorda();
+    mostraRaquete(xRaquete, yRaquete);
+    movimentaMinhaRaquete();
+    verificaColisaoRaquete(xRaquete, yRaquete);
+    mostraRaquete(xRaqueteOponente, yRaqueteOponente);
+    movimentaRaqueteOponente();
+    verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
+    incluirPlacar();
+    marcarPonto();
+}
+
+function mostraBolinha() {
+    circle(xBolinha, yBolinha, diametro)
+}
+
+function movimentaBolinha() {
+    xBolinha += velocidadeXBolinha;
+    yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda() {
+    if (xBolinha + raio > width || xBolinha - raio < 0) {
+        velocidadeXBolinha *= -1;
+    }
+    if (yBolinha + raio > height || yBolinha - raio < 0) {
+        velocidadeYBolinha *= -1;
+    }
+}
+
+function mostraRaquete(x,y){
+  rect(x, y, raqueteComprimento, raqueteAltura)
+}
+
+function movimentaMinhaRaquete(){
+  if (keyIsDown(87)){
+    yRaquete -=10;
+  }
+  if (keyIsDown(83)){
+    yRaquete +=10;
+  }
+  // Vamos limitar a movimentação da raquete para que ela não ultrapasse as bordas:
+    yRaquete = constrain(yRaquete, 0, 310);
+}
+
+function verificaColisaoRaquete(x, y) {
+    colidiu =
+collideRectCircle(x,y,raqueteComprimento,raqueteAltura,xBolinha,yBolinha,raio)
+    if (colidiu){
+        velocidadeXBolinha *= -1;
+      raquetada.play();
+    }
+}
+
+function movimentaRaqueteOponente(){
+  if (keyIsDown(UP_ARROW)){
+    yRaqueteOponente -=10;
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    yRaqueteOponente +=10;
+  }
+  //velocidadeYOponente = yBolinha -yRaqueteOponente -raqueteComprimento / 2 - 30;
+  //yRaqueteOponente += velocidadeYOponente;
+  
+  // Vamos limitar a movimentação da raquete para que ela não ultrapasse as bordas:
+    yRaqueteOponente = constrain(yRaqueteOponente, 0, 310);
+}
+
+function incluirPlacar(){
+  stroke(255)
+  textAlign(CENTER);
+  textSize(16);
+  fill(color(255, 140, 0));
+  rect(150, 10, 40, 20);
+  fill(255);
+  text(meusPontos, 170, 26);
+  fill(color(255, 140, 0));
+  rect(450, 10, 40, 20);
+  fill(255);
+  text(pontosDoOponente, 470, 26);
+}
+
+function marcarPonto(){
+  if (xBolinha > 590){
+    meusPontos += 1;
+    ponto.play();
+  }
+  if (xBolinha < 10){
+    pontosDoOponente += 1;
+    ponto.play();
+  }
+}
